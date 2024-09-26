5f70acb72bdaa4f56bac5892ea9cd1d18315e4a4
Steven Chorkawy <schorkawy@clarington.net>
2024-09-26 15:25:10 -0400

    more testing.

---
+++ TEST_SCRIPT.ps1
@@ -0,0 +1,18 @@
+Clear-Host
+
+Connect-PnPOnline -Url "https://claringtonnet.sharepoint.com/sites/StevensTestSite" -Interactive
+
+
+$Context = Get-PnPContext
+$DOCUMENT_SET_ID = "0x0120D520"
+$listName = "PSTest"
+$viewName = "Group by Status"
+$library = Get-PnPList -Identity $listName -Includes Views, ContentTypes, Title, DefaultView
+$contentTypes = $library.ContentTypes
+
+$ct = $contentTypes | Where-Object { $_.Name -eq "Document Set" }
+Set-PnPView -List $listName -Identity $viewName -Values @{AssociatedContentTypeId = $ct.Id.ToString(); ContentTypeId = $ct.Id; DefaultViewForContentType = $true }
+$Context.ExecuteQuery()
+
+$views = $library.Views | Where-Object { $_.BaseViewId -eq "1" }
+Write-Host "The End."
