396d15ec4025bbe3d830432bd71f917e430cdc7d
Steven Chorkawy <schorkawy@clarington.net>
2024-09-27 16:31:50 -0400

    Testing adding content types from CTHub.

--- TEST_SCRIPT.ps1
+++ TEST_SCRIPT.ps1
@@ -1,17 +1,36 @@
 Clear-Host
+$excelRows = Import-Excel -Path "C:\Users\sc13\OneDrive - clarington.net\Desktop\SharePoint Site Config Template.xlsx"
 
-Connect-PnPOnline -Url "https://claringtonnet.sharepoint.com/sites/StevensTestSite" -Interactive
+foreach ($row in $excelRows) {
+    Write-Host "Connecting to $($row.SiteUrl)"
+    Connect-PnPOnline -Url $row.SiteUrl -Interactive
+    $docSet_ParentContentType = Get-PnPCompatibleHubContentTypes -WebUrl $row.SiteUrl | Where-Object { $_.Group -eq "Organizational Content Types" -and $_.ParentName -eq "EDRM Document Set" -and $_.Name -match $row.DocumentSet_ParentContentType }
+    $doc_ParentContentType = Get-PnPCompatibleHubContentTypes -WebUrl $row.SiteUrl | Where-Object { $_.Group -eq "Organizational Content Types" -and $_.ParentName -eq "EDRM Document" -and $_.Name -match $row.DocumentSet_ParentContentType }
 
+    Add-PnPContentTypesFromContentTypeHub -ContentTypes @($docSet_ParentContentType.Id, $doc_ParentContentType.Id)
+}
 
-$Context = Get-PnPContext
-$DOCUMENT_SET_ID = "0x0120D520"
-$listName = "PSTest"
-$viewName = "Group by Status"
-$library = Get-PnPList -Identity $listName -Includes Views, ContentTypes, Title, DefaultView
-$contentTypes = $library.ContentTypes
+Write-Host "Site Content Types"
+Get-PnPContentType | Where-Object { $_.Group -eq "Organizational Content Types" }
 
-$ct = $contentTypes | Where-Object { $_.Name -eq "Document Set" }
-Set-PnPView -List $listName -Identity $viewName -Values @{JSLink = "hierarchytaskslist.js|customrendering.js"; AssociatedContentTypeId = $ct.Id.ToString(); ContentTypeId = $ct.Id; DefaultViewForContentType = $true }
+#######################
+#   This stuff works :) 
+# Clear-Host
 
-$views = $library.Views | Where-Object { $_.BaseViewId -eq "1" }
-Write-Host "The End."
+# $siteURL = "https://claringtonnet.sharepoint.com/sites/TemplateforCommitteeSites"
+
+# Connect-PnPOnline -Url $siteURL -Interactive
+
+# # Get content types present in content type hub site that are possible to be added to the current site.
+# $hubContentTypes = Get-PnPCompatibleHubContentTypes -WebUrl $siteURL | Where-Object { $_.Group -eq "Organizational Content Types" -and $_.Name -match "AA.05.01" }
+
+# $hubContentTypeIDs = @()
+
+# foreach ($contentType in $hubContentTypes) {
+#     $hubContentTypeIDs += $contentType.Id
+# }
+
+# Add-PnPContentTypesFromContentTypeHub -ContentTypes $hubContentTypeIDs
+
+# Get Content Types currently on site. 
+#Get-PnPContentType
