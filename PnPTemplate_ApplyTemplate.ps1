970ef6f79fc791ead8d07f30d5984a8f8ea248e0
Steven Chorkawy <schorkawy@clarington.net>
2024-10-08 09:25:47 -0400

    Adding a clear host at the start of the script.

--- PnPTemplate_ApplyTemplate.ps1
+++ PnPTemplate_ApplyTemplate.ps1
@@ -1,3 +1,4 @@
+Clear-Host
 $PnPID = "08a2a4b7-9f6f-46cb-87e7-2ed02a66fc22"
 $siteUrl = Read-Host "Enter URL of the site to deploy the template (e.g., https://Claringtonnet.sharepoint.com/sites/yoursite)"
 $Path = Read-Host "Enter the path to your site template (e.g., C:\Temp\MoCCommittee.pnp)"
@@ -20,5 +21,3 @@ foreach ($Library in $Libraries) {
     Remove-PnPContentTypeFromList -List $Library -ContentType "Document" -ErrorAction SilentlyContinue
     # Remove the '_ExtendedDescription' field as well.
 }
-
-
--- PnPTemplate_SaveTemplate.ps1
+++ PnPTemplate_SaveTemplate.ps1
@@ -1,3 +1,4 @@
+Clear-Host
 $PnPID = "08a2a4b7-9f6f-46cb-87e7-2ed02a66fc22"
 $siteUrl = Read-Host "Enter URL of the site to save as a template (e.g., https://Claringtonnet.sharepoint.com/sites/yoursite)"
 $Path = Read-Host "Enter the path where you'd like to save the site template (e.g., C:\Temp\MoCCommittee.pnp)"
