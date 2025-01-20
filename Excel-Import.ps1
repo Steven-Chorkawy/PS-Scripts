b239fd8ecec0b8a659885725f6084e5e88403dab
Steven Chorkawy <schorkawy@clarington.net>
2025-01-20 11:05:10 -0500

    Testing excel import.

---
+++ Excel-Import.ps1
@@ -0,0 +1,20 @@
+﻿Clear-Host
+$Header = 'Device Name','Depot', 'Status', 'IMEI / MEID','Serial Number', 'Model Name', 'Agent Version', 'OS Version', 'MAC Address', 'Firmware Version', 'ICCID Information', 'Last Updated', 'Last Location (UTC-05:00)',	'Owner'
+$data = Import-Csv -Path "C:\Users\sc13\Downloads\CSV_FOCUS Devices.csv"
+
+Connect-PnPOnline -Url "https://claringtonnet.sharepoint.com/sites/InfoTech" -Interactive
+
+
+ForEach($item in $data) {
+    Write-Host $item.'Device Name'
+
+    Add-PnPListItem -List "Hardware Inventory" -ContentType "Mobile Device Item" -Values @{
+        "Title" = $item.'Device Name';
+        "Status" = $item.'Status';
+        "SIMCards" = $item.'ICCID Information';
+        "Model" = $item.'Model Name';
+        "MACAddress" = $item.'MAC Address';
+        "IMEI" = $item.'IMEI / MEID';
+        "CurrentOwner" = $item.'Owner'
+    }
+}
--- TEST_SCRIPT.ps1
+++ TEST_SCRIPT.ps1
@@ -1,8 +1,25 @@
-Clear-Host
+#Connect to PnP Online
+Connect-PnPOnline -Url "https://claringtonnet-admin.sharepoint.com" -Interactive
+   
+$allSites = Get-PnPTenantSite | Where-Object { $_.Status -eq "Active" -and ($_.Template -eq "SITEPAGEPUBLISHING#0" -or $_.Template -eq "STS#3") } | Select-Object -Property Title, Url, Status, Owner, Template, IsTeamsConnected, IsTeamsChannelConnected
 
-Connect-PnPOnline -Url "https://claringtonnet.sharepoint.com/sites/DocSet_Test_3" -Interactive
+$allSites | Export-Csv -Path "C:\Users\sc13\OneDrive - clarington.net\Desktop\Excel Export\Disable_Members_Edit_Membership.csv"
 
-$ct = Get-PnPContentType -List "Administration" -Includes Parent, Fields | Where-Object { $_.Name -like "*Case" }
+#Enable Members can Sharing
+$Web = Get-PnPWeb
+$Web.MembersCanShare = $true
+$Web.Update()
+Invoke-PnPQuery
+ 
+#Update the Members Group - Members can share Files and Folders. But ONLY Site owners can share site!
+$MembersGroup = Get-PnPGroup -AssociatedMemberGroup
+Set-PnPGroup -Identity $MembersGroup -AllowMembersEditMembership:$false
 
+foreach ($site in $allSites) {
+    Write-Host $site.Title
+    Connect-PnPOnline -Url $site.Url -Interactive
+    $Web = Get-PnPWeb
+    Write-Host $Web.Url
+}
 
-Write-Host $ct.Fields[10]
+Write-Host "The End."
