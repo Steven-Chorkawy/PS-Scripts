ea4f9350a2a18c2ddc014459b734316896a09df0
Steven-Chorkawy <schorkawy@clarington.net>
2025-02-11 09:12:27 -0500

    Removing field requirement.

--- PnPTemplate_SaveTimeTrackerList.ps1
+++ PnPTemplate_SaveTimeTrackerList.ps1
@@ -4,5 +4,5 @@ $siteUrl = Read-Host "Enter URL of the site to save as a template (e.g., https:/
 $Path = Read-Host "Enter the path where you'd like to save the site template (e.g., C:\Temp\MoCCommittee.pnp)"
 
 Connect-PnPOnline -url $siteUrl -ClientId $PnPID -Interactive
-Get-PnPSiteTemplate -out $Path -ListsToExtract "TimesheetTasks", "TimesheetProjects", "Timesheet" -ExcludeHandlers ApplicationLifecycleManagement, AuditSettings, ComposedLook, CustomActions, ExtensibilityProviders, Features, Files, ImageRenditions, Navigation, PageContents, Pages, PropertyBagEntries, Publishing, RegionalSettings, SearchSettings, SiteFooter, SiteHeader, SitePolicy, SiteSecurity, SiteSettings, SupportedUILanguages, SyntexModels, Tenant, TermGroups, Theme, WebApiPermissions, WebSettings, Workflows -Force
+Get-PnPSiteTemplate -out $Path -ListsToExtract "TimesheetTasks", "TimesheetProjects", "Timesheet" -ExcludeHandlers ApplicationLifecycleManagement, AuditSettings, ComposedLook, CustomActions, ExtensibilityProviders, Features, Fields, Files, ImageRenditions, Navigation, PageContents, Pages, PropertyBagEntries, Publishing, RegionalSettings, SearchSettings, SiteFooter, SiteHeader, SitePolicy, SiteSecurity, SiteSettings, SupportedUILanguages, SyntexModels, Tenant, TermGroups, Theme, WebApiPermissions, WebSettings, Workflows -Force
 #-ExcludeHandlers SiteSecurityTime
